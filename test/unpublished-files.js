ad5e6e3776cb3e2b396e7d3f5a6a7a4b5fa0b83e
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-08-02 16:40:28 +0000

    Downgrade to ava 2.3.0

--- test/unpublished-files.js
+++ test/unpublished-files.js
@@ -8,17 +8,23 @@ test.before(async t => {
 	await t.throwsAsync(gitUtil.latestTag(), null, 'prerequisites not met: repository should not contain any tags');
 });
 
+test.afterEach.always(async t => {
+	if (typeof t.context.teardown === 'function') {
+		await t.context.teardown();
+	}
+});
+
 test.serial('file `new` to package with tags added', async t => {
 	await execa('git', ['tag', 'v0.0.0']);
 	await execa('touch', ['new']);
 	await execa('git', ['add', 'new']);
 	await execa('git', ['commit', '-m', 'new file added']);
 
-	t.teardown(async () => {
+	t.context.teardown = async () => {
 		await execa('git', ['rm', 'new']);
 		await execa('git', ['tag', '-d', 'v0.0.0']);
 		await execa('git', ['commit', '-m', 'new file deleted']);
-	});
+	};
 
 	t.deepEqual(await util.getNewAndUnpublishedFiles({files: ['*.js']}), ['new']);
 });
@@ -26,8 +32,9 @@ test.serial('file `new` to package with tags added', async t => {
 test.serial('file `new` to package without tags added', async t => {
 	await execa('touch', ['new']);
 
-	t.teardown(() => {
+	t.context.teardown = () => {
 		fs.unlinkSync('new');
-	});
+	};
+
 	t.deepEqual(await util.getNewAndUnpublishedFiles({files: ['*.js']}), ['new']);
 });
