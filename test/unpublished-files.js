1771d17d524bb33a4cfc99df36000a4e460fcde5
Bunyanuch Saengnet <bunyanuch.saengnet@kabelsurfer.com>
2020-11-14 10:50:00 +0000

    add test-case with long directory names

--- test/unpublished-files.js
+++ test/unpublished-files.js
@@ -3,6 +3,7 @@ const execa = require('execa');
 const util = require('../../source/util.js');
 const gitUtil = require('../../source/git-util.js');
 const fs = require('fs');
+const path = require('path');
 
 test.before(async t => {
 	await t.throwsAsync(gitUtil.latestTag(), null, 'prerequisites not met: repository should not contain any tags');
@@ -37,4 +38,22 @@ test.serial('file `new` to package without tags added', async t => {
 	};
 
 	t.deepEqual(await util.getNewAndUnpublishedFiles({files: ['*.js']}), ['new']);
+});
+
+test.serial('files with long pathnames added', async t => {
+	const longPath = path.join('veryLonggggggDirectoryName', 'veryLonggggggDirectoryName');
+	await execa('git', ['tag', 'v0.0.0']);
+	await execa('mkdir', ['-p', longPath]);
+	await execa('touch', [path.join(longPath, 'file1')]);
+	await execa('touch', [path.join(longPath, 'file2')]);
+	await execa('git', ['add', '.']);
+	await execa('git', ['commit', '-m', 'new files added']);
+
+	t.context.teardown = async () => {
+		await execa('git', ['rm', '-r', longPath]);
+		await execa('git', ['tag', '-d', 'v0.0.0']);
+		await execa('git', ['commit', '-m', 'new files deleted']);
+	};
+
+	t.deepEqual(await util.getNewAndUnpublishedFiles({files: ['*.js']}), [path.join(longPath, 'file1'), path.join(longPath, 'file2')]);
 });
